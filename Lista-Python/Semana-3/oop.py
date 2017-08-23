1f2288c3d3285b08a133c4f029ed3a3c09cdde59
Sandro J. Torres <sandrotorres.ti@gmail.com>
2017-08-23 10:25:25 -0300

    Lista de exercício Python
    Lista de atividade desenvolvidas na Faculdade

---
+++ Lista-Python/Semana-3/oop.py
@@ -0,0 +1,15 @@
+import datetime
+class Pessoa():
+  def __init__(self, nome, nascimento):
+    self.nome = nome
+    self.nascimento = nascimento
+  def idade(self):
+    delta = datetime.date.today() - self.nascimento
+    return int(delta.days/365)
+  def __str__( self ):
+    return '%s, %d anos' %(self.nome, self.idade())
+
+masanori = Pessoa('Fernando Masanori', datetime.date(1980, 9, 1))
+print (masanori.idade())
+print (masanori)
+
