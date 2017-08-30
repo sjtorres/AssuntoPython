70536f99b338ac36f4c197e5022229d7cccf249e
Sandro J. Torres <sandrotorres.ti@gmail.com>
2017-08-30 09:10:52 -0300

    Atividade 03 Python
    Classe Pessoa com Herança (Cliente e Fornecedor)

---
+++ PessoaClienteFornecedor-Python/mainClass.py
@@ -0,0 +1,10 @@
+class Pessoa:
+	def __init__(self, nome, endereco, telefone, email)
+		self.nome = nome
+		self.endereco = endereco
+		self.telefone = telefone
+		self.email = email
+
+	def imprimir(self)
+		print('Meu nome é %s , moro %s e meu e-mail é %s' %(self.nome, self.endereco, self.email))
+		
